bea23d3fe9b3e019f5d84ec33b0c9c0b915c7d7c
Isidro Lopez <islomar@gmail.com>
2024-02-14 13:50:46 +0100

    Toggle test names to camel case

--- tests/test_parrot.py
+++ tests/test_parrot.py
@@ -1,42 +1,42 @@
 from src.parrot import Parrot, ParrotType
 
 
-def test_speedOfEuropeanParrot() -> None:
+def test_speed_of_european_parrot() -> None:
     parrot = Parrot(ParrotType.EUROPEAN, 0, 0, False)
     assert parrot.speed() == 12.0
 
 
-def test_cryOfEuropeanParrot() -> None:
+def test_cry_of_european_parrot() -> None:
     parrot = Parrot(ParrotType.EUROPEAN, 0, 0, False)
     assert parrot.cry() == "Sqoork!"
 
 
-def test_speedOfAfricanParrot_With_One_Coconut() -> None:
+def test_speed_of_african_parrot_with_one_coconut() -> None:
     parrot = Parrot(ParrotType.AFRICAN, 1, 0, False)
     assert parrot.speed() == 3.0
 
 
-def test_cryOfAfricanParrot() -> None:
+def test_cry_of_african_parrot() -> None:
     parrot = Parrot(ParrotType.AFRICAN, 1, 0, False)
     assert parrot.cry() == "Sqaark!"
 
 
-def test_speedOfAfricanParrot_With_Two_Coconuts() -> None:
+def test_speed_of_african_parrot_with_two_coconuts() -> None:
     parrot = Parrot(ParrotType.AFRICAN, 2, 0, False)
     assert parrot.speed() == 0.0
 
 
-def test_speedOfAfricanParrot_With_No_Coconuts() -> None:
+def test_speed_of_african_parrot_with_no_coconuts() -> None:
     parrot = Parrot(ParrotType.AFRICAN, 0, 0, False)
     assert parrot.speed() == 12.0
 
 
-def test_speedNorwegianBlueParrot_nailed() -> None:
+def test_speed_norwegian_blue_parrot_nailed() -> None:
     parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, True)
     assert parrot.speed() == 0.0
 
 
-def test_speedNorwegianBlueParrot_not_nailed() -> None:
+def test_speed_norwegian_blue_parrot_not_nailed() -> None:
     parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, False)
     assert parrot.speed() == 18.0
 
