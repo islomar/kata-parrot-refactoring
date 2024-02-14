8b2150db8a147009db8d2c1990def8021c756dae
Isidro Lopez <islomar@gmail.com>
2024-02-14 14:32:56 +0100

    Replace in tests generic Parrtos with AfricanParrot

--- tests/test_parrot.py
+++ tests/test_parrot.py
@@ -12,27 +12,22 @@ def test_cry_of_european_parrot() -> None:
 
 
 def test_speed_of_african_parrot_with_one_coconut() -> None:
-    parrot = Parrot(ParrotType.AFRICAN, 1, 0, False)
-    assert parrot.speed() == 3.0
-
-
-def test_speed_of_african_parrot_with_one_coconut_v2() -> None:
     parrot = AfricanParrot(1, 0, False)
     assert parrot.speed() == 3.0
 
 
 def test_cry_of_african_parrot() -> None:
-    parrot = Parrot(ParrotType.AFRICAN, 1, 0, False)
+    parrot = AfricanParrot(1, 0, False)
     assert parrot.cry() == "Sqaark!"
 
 
 def test_speed_of_african_parrot_with_two_coconuts() -> None:
-    parrot = Parrot(ParrotType.AFRICAN, 2, 0, False)
+    parrot = AfricanParrot(2, 0, False)
     assert parrot.speed() == 0.0
 
 
 def test_speed_of_african_parrot_with_no_coconuts() -> None:
-    parrot = Parrot(ParrotType.AFRICAN, 0, 0, False)
+    parrot = AfricanParrot(0, 0, False)
     assert parrot.speed() == 12.0
 
 
