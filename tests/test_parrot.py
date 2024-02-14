db146895f44a2a733c6a3872cc8b3e35d952a0ce
Isidro Lopez <islomar@gmail.com>
2024-02-14 14:40:34 +0100

    Replace in tests generic Parrot with NorwegianBlueParrot

--- tests/test_parrot.py
+++ tests/test_parrot.py
@@ -1,4 +1,4 @@
-from src.parrot import Parrot, ParrotType, EuropeanParrot, AfricanParrot
+from src.parrot import EuropeanParrot, AfricanParrot, NorwegianBlueParrot
 
 
 def test_speed_of_european_parrot() -> None:
@@ -32,25 +32,25 @@ def test_speed_of_african_parrot_with_no_coconuts() -> None:
 
 
 def test_speed_norwegian_blue_parrot_nailed() -> None:
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, True)
+    parrot = NorwegianBlueParrot(0, 1.5, True)
     assert parrot.speed() == 0.0
 
 
 def test_speed_norwegian_blue_parrot_not_nailed() -> None:
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, False)
+    parrot = NorwegianBlueParrot(0, 1.5, False)
     assert parrot.speed() == 18.0
 
 
 def test_speed_norwegian_blue_parrot_not_nailed_high_voltage() -> None:
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 4, False)
+    parrot = NorwegianBlueParrot(0, 4, False)
     assert parrot.speed() == 24.0
 
 
 def test_cry_norwegian_blue_parrot_high_voltage() -> None:
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1, False)
+    parrot = NorwegianBlueParrot(0, 1, False)
     assert parrot.cry() == "Bzzzzzz"
 
 
 def test_cry_norwegian_blue_parrot_no_voltage() -> None:
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 0, False)
+    parrot = NorwegianBlueParrot(0, 0, False)
     assert parrot.cry() == "..."
